b904f68cedb28684ae999cc62ad724cb6d81aa63
mohamedrawther <mohamedrawther@gmail.com>
2023-10-17 22:07:34 +0530

    stream changes

--- src/main/java/com/jf/tutor/stream/data/Address.java
+++ src/main/java/com/jf/tutor/stream/data/Address.java
@@ -1,8 +1,10 @@
 package com.jf.tutor.stream.data;
 
+import lombok.Data;
 
+@Data
 non-sealed public class Address extends AgentBase{
-	
+
 	public Address() {
 		
 	}
@@ -23,4 +25,51 @@ non-sealed public class Address extends AgentBase{
 	private String country;
 	private String city;
 	private String pincode;
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
 }
